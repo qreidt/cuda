0b24a2d7442d8a48f79e43da7d4a42d028615381
Caio Reidt <caio.reidt@garnon.me>
2019-12-31 19:54:42 -0300

    Atualização do algoritmo para o uso de threads normais

--- app.py
+++ app.py
@@ -10,6 +10,8 @@
 # VALOR TOTAL DA COMBINAÇÃO E LISTA DE OBJETOS PARA ATINGIR TAL COMBINAÇÃO)
 
 from builtins import range, len, enumerate
+from math import ceil, floor
+from threading import Thread
 
 
 class Objeto:  # CLASSE PARA FACILITAR A MANIPULAÇÃO DE ITENS
@@ -34,63 +36,115 @@ class Mochila:  # CLASSE PARA MONTAR COMBINAÇÕES DE OBJETOS (BASICAMENTE A MOC
         return mochila
 
     def __repr__(self):
-        return 'Valor: %i, %i Objetos' % (self.valor, len(self.objetos))
+        # return 'Valor: %i, %i Objetos' % (self.valor, len(self.objetos))
+        return 'Combinacao(%i)' % self.valor
 
 
-def knapSack(objs, we):
+def knapSackThread(start, end, i, obj):
+    global K
+
+    # PERCORRER A LINHA DE COMBINAÇÕES PULANDO A
+    # PRIMEIRA COLUNA DA LINHA
+    for j in range(start, end + 1):
+
+        # LIMITAR O TAMANHO DO OBJETO QUE VAI ENTRAR NA COMBINAÇÃO
+        # A MEDIDA QUE ELE VAI PERCORRENDO A LINHA, ELE LIBERA OBJETOS MAIORES
+        # E A COMBINAÇÃO É FEITA COM OBJETOS MENORES DA LINHA ANTERIOR ( POR ISSO MANTER A PRIMEIRA LINHA VAZIA )
+        if obj.peso <= j:
+
+            # CALCULAR NOVO VALOR
+            value = obj.valor + K[i - 1][j - obj.peso].valor
+
+            # SE O NOVO VALOR FOR MAIOR DO QUE O OBTIDO NA LINHA ANTERIOR
+            if value > K[i - 1][j].valor:
+
+                # ADICIONAR OBJETO À COMBINAÇÃO DA LINHA ANTERIOR
+                K[i][j] = K[i - 1][j - obj.peso].addObjeto(obj)
+
+            # SE NÃO, DEIXE COMO ESTÁ
+            else:
+                K[i][j] = K[i - 1][j]
+
+        # DEIXAR COMO ESTÁ TAMBÉM
+        else:
+            K[i][j] = K[i - 1][j]
+
+
+def knapSack(num_threads):
+    global OBJS, K, W
+
+    # EXTENDER O ARRAY DE OBJETOS
+    OBJS.insert(0, OBJS[0])
+
     # GERAR UMA MATRIZ DE n_itens + 1 x tamanho_da_mochila + 1
     # ESTA MATRIZ É A BASE DO ALGORITMO O-1 DO PROBLEMA DA MOCHILA
     # EXPLICAÇÃO À PARTIR DOS @21:00 LINK: https://www.youtube.com/watch?v=SJSRKnxu9Ig
     # PREENCHER A MATRIZ COM CLASSES DE COMBINAÇÃO
-    k = [[Mochila() for x in range(we + 1)] for x in range(len(objs) + 1)]
+    K = [[Mochila() for x in range(W + 1)] for x in range(len(OBJS))]
 
-    # EXTENDER O ARRAY DE OBJETOS
-    objs = [objs[0]] + objs
+    # REALIZAR A DIVISÃO DE QUANTOS ITENS CADA THREAD IRA CALCULAR DE CADA LINHA
+    per_thread = W / NUM_THREADS
+
+    # CASO SEJA UM NÚMERO INTEIRO, A ULTIMA THREAD IRA TER A MESMA QUANTUDADE QUE AS OUTRAS
+    if per_thread == ceil(per_thread):
+        last_thread = per_thread - 1
+        per_thread = per_thread - 1
+
+    # CASO NÃO SEJA, A ULTIMA THREAD IRÁ RECEBER UM ITEM A MAIS POR CADA LINHA
+    else:
+        last_thread = ceil(per_thread) - 1
+        per_thread = floor(per_thread) - 1
+
+    # PASSAR AS VARIAVEIS DE FLOAT PARA INT
+    per_thread = int(per_thread)
+    last_thread = int(last_thread)
+
+    # INICIAR ARRAY DE THREADS
+    threads = []
+    for t in range(num_threads):
+        threads.append(0)
 
     # TRABALHAR NA MATRIZ K[][] DE MANEIRA BOTTOM UP
     # PULANDO A PRIMEIRA LINHA DA MATRIZ
-    for i, obj in enumerate(objs[1:], start=1):
+    for i, obj in enumerate(OBJS[1:], start=1):
 
-        # PERCORRER A LINHA DE COMBINAÇÕES PULANDO A
-        # PRIMEIRA COLUNA DA LINHA
-        for j, combinacao in enumerate(k[i][1:], start=1):
+        start = 1  # INICIAR SEMPRE NA SEGUNDA COLUNA
+        end = start + per_thread
 
-            # LIMITAR O TAMANHO DO OBJETO QUE VAI ENTRAR NA COMBINAÇÃO
-            # A MEDIDA QUE ELE VAI PERCORRENDO A LINHA, ELE LIBERA OBJETOS MAIORES
-            # E A COMBINAÇÃO É FEITA COM OBJETOS MENORES DA LINHA ANTERIOR ( POR ISSO MANTER A PRIMEIRA LINHA VAZIA )
-            if obj.peso <= j:
+        # INICIAR THREADS E IR PASSANDO O INTERVALO DE CADA UMA
+        for t, thread in enumerate(threads):
+            # INICIAR A THREAD PASSANDO QUANDO FUNÇÃO ELA DEVE EXECUTAR E SEUS PARÂMETROS
+            threads[t] = Thread(target=knapSackThread, args=(start, end, i, obj))
 
-                # CALCULAR NOVO VALOR
-                value = obj.valor + k[i - 1][j - obj.peso].valor
+            # TACA-LE PAU NA THREAD
+            threads[t].start()
 
-                # SE O NOVO VALOR FOR MAIOR DO QUE O OBTIDO NA LINHA ANTERIOR
-                if value > k[i - 1][j].valor:
-
-                    # ADICIONAR OBJETO À COMBINAÇÃO DA LINHA ANTERIOR
-                    k[i][j] = k[i - 1][j - obj.peso].addObjeto(obj)
-
-                # SE NÃO, DEIXE COMO ESTÁ
-                else:
-                    k[i][j] = k[i - 1][j]
-
-            # DEIXAR COMO ESTÁ TAMBÉM
+            # ATUALIZAR INTERVALO
+            start = start + end
+            if t + 2 == num_threads and per_thread != last_thread:
+                end = start + last_thread
             else:
-                k[i][j] = k[i - 1][j]
+                end = start + per_thread
+
+        # JUNTAR TODAS AS THREADS À THREAD PRINCIPAL
+        for thread in threads:
+            thread.join()
 
     # RETORNAR ÚLTIMA COMBINAÇÃO DA MATRIZ
-    return k[len(objs) - 1][we]
+    return K[len(OBJS) - 1][W]
 
 
 # VALORES PARA TESTAR O ALGORITMO
 
+NUM_THREADS = 2  # NÚMEDO DE THREADS
 W = 550  # PESO MÁXIMO DA MOCHILA
 
 # LISTA DE OBJETOS
 OBJS = [
-    # Objeto(1, 6, 1),
-    # Objeto(2, 10, 2),
-    # Objeto(3, 12, 3),
-    # Objeto(4, 23, 5)
+    # Objeto(1, 1, 8),
+    # Objeto(2, 2, 10),
+    # Objeto(3, 3, 12),
+    # Objeto(4, 23, 5),
     Objeto(1, 26, 27),
     Objeto(2, 17, 31),
     Objeto(3, 23, 34),
@@ -118,11 +172,15 @@ OBJS = [
     Objeto(25, 18, 29),
     Objeto(26, 31, 22),
     Objeto(27, 35, 17),
-    Objeto(28, 15, 9),
+    Objeto(28, 15, 9)
 ]
 
+# OBS: TODAS AS VARIAVEIS DECLARADAS AQUI PODEM SER CONSIDERADAS GLOBAIS
+
+K = []
+
 # INICIAR O ALGORITMO COM OS VALORES DEFINIDOS ACIMA
-comb = knapSack(OBJS, W)
+comb = knapSack(NUM_THREADS)
 
 peso_total = 0
 for item in comb.objetos:
@@ -132,5 +190,6 @@ for item in comb.objetos:
 print('Valor: %i' % comb.valor)
 print('Peso Total: %i' % peso_total)
 print('Objetos: %i' % len(comb.objetos))
+
 for objeto in comb.objetos:
     print('\t' + 'ID: %i, Valor: %i, Peso: %i' % (objeto.id, objeto.valor, objeto.peso))
